062b112cce9375dceb03ab4d4d7e142ef711e528
Simon Howard <fraggle@soulsphere.org>
2022-05-27 23:17:32 -0400

    Initial code for better error checking.

--- yocton.c
+++ yocton.c
@@ -7,9 +7,11 @@
 
 #include "yocton.h"
 
+#define ERROR_ALLOC "memory allocation failure"
+#define ERROR_EOF   "unexpected EOF"
+
 #define CHECK_OR_GOTO_FAIL(condition) \
 	if (!(condition)) { goto fail; }
-
 
 enum token_type {
 	TOKEN_STRING,
@@ -21,14 +23,39 @@ enum token_type {
 };
 
 struct yocton_instream {
+	// callback gets invoked to read more data from input.
 	yocton_read callback;
 	void *callback_handle;
-	char *buf, *string;
+	// buf is the input buffer containing last data read by callback.
+	// buf[buf_offset:buf_len] is still to read.
+	char *buf;
 	size_t buf_len, buf_size;
-	size_t string_len, string_size;
 	unsigned int buf_offset;
+	// string contains the last string token read.
+	char *string;
+	size_t string_len, string_size;
+	// error is non-NULL if an error occurs during parsing.
+	char *error;
+	int lineno;
 	struct yocton_object *root;
 };
+
+static void input_error(struct yocton_instream *s, char *fmt, ...)
+{
+	// TODO
+	s->error = "error";
+}
+
+// Assign the result of an allocation, storing an error if result == NULL.
+static int assign_alloc(void *ptr, struct yocton_instream *s, void *result)
+{
+	if (result == NULL) {
+		input_error(s, ERROR_ALLOC);
+		return 0;
+	}
+	* ((void **) ptr) = result;
+	return 1;
+}
 
 static int peek_next_char(struct yocton_instream *s, char *c)
 {
@@ -44,12 +71,17 @@ static int peek_next_char(struct yocton_instream *s, char *c)
 	return 1;
 }
 
+// Read next char from input. Failure stores an error.
 static int read_next_char(struct yocton_instream *s, char *c)
 {
 	if (!peek_next_char(s, c)) {
+		input_error(s, ERROR_EOF);
 		return 0;
 	}
 	++s->buf_offset;
+	if (*c == '\n') {
+		++s->lineno;
+	}
 	return 1;
 }
 
@@ -60,19 +92,18 @@ static int is_bare_string_char(int c)
 
 static int append_string_char(struct yocton_instream *s, char c)
 {
-	char *new_stringbuf;
 	if (s->string_len + 1 >= s->string_size) {
 		s->string_size = s->string_size == 0 ? 64 : s->string_size * 2;
-		new_stringbuf = realloc(s->string, s->string_size);
-		if (new_stringbuf == NULL) {
-			return 0;
-		}
-		s->string = new_stringbuf;
+		CHECK_OR_GOTO_FAIL(
+		    assign_alloc(&s->string, s,
+		        realloc(s->string, s->string_size)));
 	}
 	s->string[s->string_len] = c;
 	s->string[s->string_len + 1] = '\0';
 	++s->string_len;
 	return 1;
+fail:
+	return 0;
 }
 
 static int unescape_string_char(char *c) {
@@ -101,43 +132,58 @@ static enum token_type read_string(struct yocton_instream *s)
 			return TOKEN_STRING;
 		} else if (c == '\\') {
 			CHECK_OR_GOTO_FAIL(read_next_char(s, &c));
-			CHECK_OR_GOTO_FAIL(unescape_string_char(&c));
+			if (!unescape_string_char(&c)) {
+				input_error(s, "unknown string escape: \\%c", c);
+				return TOKEN_ERROR;
+			}
 		}
 		CHECK_OR_GOTO_FAIL(append_string_char(s, c));
 	}
 fail:
-	// TODO: More detailed error reporting
+	return TOKEN_ERROR;
+}
+
+static enum token_type read_bare_string(struct yocton_instream *s, char first)
+{
+	char c;
+	if (!is_bare_string_char(first)) {
+		input_error(s, "unknown token: not valid bare-string character");
+		return TOKEN_ERROR;
+	}
+	s->string_len = 0;
+	CHECK_OR_GOTO_FAIL(append_string_char(s, first));
+	// Reaching EOF in the middle of the string is explicitly okay here:
+	while (peek_next_char(s, &c) && is_bare_string_char(c)) {
+		CHECK_OR_GOTO_FAIL(read_next_char(s, &c));
+		CHECK_OR_GOTO_FAIL(append_string_char(s, c));
+	}
+	return TOKEN_STRING;
+fail:
 	return TOKEN_ERROR;
 }
 
 static enum token_type read_next_token(struct yocton_instream *s)
 {
 	char c;
+	if (s->error != NULL) {
+		return TOKEN_ERROR;
+	}
+	// Skip past any spaces. Reaching EOF is not always an error.
 	do {
-		if (!read_next_char(s, &c)) {
+		if (!peek_next_char(s, &c)) {
 			return TOKEN_EOF;
 		}
+		CHECK_OR_GOTO_FAIL(read_next_char(s, &c));
 	} while (isspace(c));
-	s->string_len = 0;
-	CHECK_OR_GOTO_FAIL(append_string_char(s, c));
+
 	switch (c) {
 		case ':':  return TOKEN_COLON;
 		case '{':  return TOKEN_OPEN_BRACE;
 		case '}':  return TOKEN_CLOSE_BRACE;
 		case '\"': return read_string(s);
-		default:   break;
+		default:   return read_bare_string(s, c);
 	}
-	CHECK_OR_GOTO_FAIL(is_bare_string_char(c));
-	for (;;) {
-		if (!peek_next_char(s, &c) || !is_bare_string_char(c)) {
-			break;
-		}
-		CHECK_OR_GOTO_FAIL(read_next_char(s, &c));
-		CHECK_OR_GOTO_FAIL(append_string_char(s, c));
-	}
-	return TOKEN_STRING;
 fail:
-	// TODO: More detailed error reporting
 	return TOKEN_ERROR;
 }
 
@@ -215,11 +261,14 @@ struct yocton_object *yocton_read_with(yocton_read callback, void *handle)
 	instream->root = obj;
 	instream->callback = callback;
 	instream->callback_handle = handle;
-	instream->buf_size = 256;
+	instream->error = NULL;
+	instream->lineno = 1;
 	instream->buf_len = 0;
 	instream->buf_offset = 0;
-	instream->buf = calloc(instream->buf_size, sizeof(char));
-	CHECK_OR_GOTO_FAIL(instream->buf != NULL);
+	instream->buf_size = 256;
+	CHECK_OR_GOTO_FAIL(
+	    assign_alloc(&instream->buf, instream,
+	        calloc(instream->buf_size, sizeof(char))));
 	instream->string_size = 0;
 	instream->string = NULL;
 
@@ -261,36 +310,42 @@ static struct yocton_field *next_field(struct yocton_object *obj)
 {
 	struct yocton_field *f = NULL;
 
-	f = calloc(1, sizeof(struct yocton_field));
-	CHECK_OR_GOTO_FAIL(f != NULL);
+	CHECK_OR_GOTO_FAIL(
+	    assign_alloc(&f, obj->instream,
+	        calloc(1, sizeof(struct yocton_field))));
 	obj->field = f;
-	f->name = strdup(obj->instream->string);
-	CHECK_OR_GOTO_FAIL(f->name != NULL);
+	CHECK_OR_GOTO_FAIL(
+	    assign_alloc(&f->name, obj->instream,
+	        strdup(obj->instream->string)));
 
 	switch (read_next_token(obj->instream)) {
 		case TOKEN_COLON:
 			// This is the string:string case.
 			f->type = YOCTON_FIELD_STRING;
+			if (read_next_token(obj->instream) != TOKEN_STRING) {
+				input_error(obj->instream, "string expected "
+				            "to follow ':'");
+				goto fail;
+			}
 			CHECK_OR_GOTO_FAIL(
-				read_next_token(obj->instream) == TOKEN_STRING);
-			f->value = strdup(obj->instream->string);
-			CHECK_OR_GOTO_FAIL(f->value != NULL);
+			    assign_alloc(&f->value, obj->instream,
+			        strdup(obj->instream->string)));
 			return f;
 		case TOKEN_OPEN_BRACE:
 			f->type = YOCTON_FIELD_OBJECT;
-			f->child = calloc(1, sizeof(struct yocton_object));
-			CHECK_OR_GOTO_FAIL(f->child != NULL);
+			CHECK_OR_GOTO_FAIL(
+			    assign_alloc(&f->child, obj->instream,
+			        calloc(1, sizeof(struct yocton_object))));
 			f->child->instream = obj->instream;
 			f->child->done = 0;
 			return f;
 		default:
-			// TODO: Save error
-			assert(0);
-			return NULL;
+			input_error(obj->instream, "':' or '{' expected to "
+			            "follow field name");
+			goto fail;
 	}
 fail:
 	free_field(f);
-	// TODO: Save error
 	return NULL;
 }
 
@@ -308,23 +363,26 @@ struct yocton_field *yocton_next_field(struct yocton_object *obj)
 		case TOKEN_STRING:
 			return next_field(obj);
 		case TOKEN_CLOSE_BRACE:
-			CHECK_OR_GOTO_FAIL(obj != obj->instream->root);
+			if (obj == obj->instream->root) {
+				input_error(obj->instream, "closing brace "
+				            "'}' not expected at top level");
+				return NULL;
+			}
 			obj->done = 1;
 			return NULL;
 		case TOKEN_EOF:
 			// EOF is only valid at the top level.
-			if (obj == obj->instream->root) {
-				obj->done = 1;
+			if (obj != obj->instream->root) {
+				input_error(obj->instream, ERROR_EOF);
 				return NULL;
 			}
-			/* fallthrough */
+			obj->done = 1;
+			return NULL;
 		default:
-			assert(0);
+			input_error(obj->instream, "expected start of "
+			            "next field");
 			return NULL;
 	}
-fail:
-	assert(0);
-	return NULL;
 }
 
 enum yocton_field_type yocton_field_type(struct yocton_field *f)
