aa1cfd5d703f2b52308f60f5ee130c1223c61322
Simon Howard <fraggle@soulsphere.org>
2022-06-10 22:32:06 -0400

    Add 'special.' prefix for read_as_* fields.

--- yocton_test.c
+++ yocton_test.c
@@ -96,9 +96,9 @@ void evaluate_obj(struct yocton_object *obj)
 			                &throwaway));
 		}
 		ft = yocton_field_type(field);
-		if (!strcmp(name, "read_as_object")) {
+		if (!strcmp(name, "special.read_as_object")) {
 			ft = YOCTON_FIELD_OBJECT;
-		} else if (!strcmp(name, "read_as_string")) {
+		} else if (!strcmp(name, "special.read_as_string")) {
 			ft = YOCTON_FIELD_STRING;
 		}
 		if (ft == YOCTON_FIELD_OBJECT) {
