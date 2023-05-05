acc5b29557a6e0d70fcb3c6b27f0bddb192c94c1
Simon Howard <fraggle@soulsphere.org>
2023-05-05 19:05:32 -0400

    Add doc comments

--- yoctonw.h
+++ yoctonw.h
@@ -18,6 +18,24 @@
 
 #include <inttypes.h>
 
+/**
+ * @file yoctonw.h
+ *
+ * Functions for writing a Yocton file. The entrypoint is to use
+ * @ref yocton_write_with or @ref yocton_write_from.
+ */
+
+/**
+ * Callback invoked to write more data to the output.
+ *
+ * @param buf       Buffer containing data to write.
+ * @param buf_size  Size of buffer in bytes.
+ * @param handle    Arbitrary pointer, passed through from
+ *                  @ref yocton_write_with.
+ * @return          1 for success; 0 for failure. If a failure status
+ *                  is returned, the callback will not be invoked
+ *                  again.
+ */
 typedef int (*yoctonw_write)(void *buf, size_t nbytes, void *handle);
 
 struct yoctonw_buffer {
@@ -27,18 +45,89 @@ struct yoctonw_buffer {
 
 struct yoctonw_writer;
 
+/**
+ * Start writing a new stream of yocton-encoded data, using the given
+ * callback to write more data.
+ *
+ * @param callback   Callback function to invoke to write data.
+ * @param handle     Arbitrary pointer passed through when callback is
+ *                   invoked.
+ * @return           A @ref yoctonw_writer that can be used to output data.
+ */
 struct yoctonw_writer *yoctonw_write_with(yoctonw_write callback, void *handle);
+
+/**
+ * Start writing a new stream of yocton-encoded data, using the given
+ * FILE handle.
+ *
+ * @param fstream    File handle.
+ * @return           A @ref yoctonw_writer that can be used to output data.
+ */
 struct yoctonw_writer *yoctonw_write_to(FILE *fstream);
+
+/**
+ * Free the writer and stop writing the output stream.
+ *
+ * @param w  The @ref yoctonw_writer.
+ */
 void yoctonw_free(struct yoctonw_writer *w);
 
+/**
+ * Write a new field and value to the output. The name and value can be
+ * any arbitrary byte sequence.
+ *
+ * @param w       Writer.
+ * @param name    Field name.
+ * @param value   Field value.
+ */
 void yoctonw_field_bytes(struct yoctonw_writer *w,
                          const struct yoctonw_buffer *name,
                          const struct yoctonw_buffer *value);
+
+/**
+ * Write a new field and value to the output.
+ *
+ * @param w       Writer.
+ * @param name    Field name.
+ * @param value   Field value.
+ */
 void yoctonw_field(struct yoctonw_writer *w, const char *name,
                    const char *value);
+
+/**
+ * Start writing a new subobject.
+ *
+ * The @ref yoctonw_end function should be called to end the subobject.
+ * The field name can be any arbitrary byte sequence.
+ *
+ * @param w       Writer.
+ * @param name    Field name for subobject.
+ */
 void yoctonw_subobject_bytes(struct yoctonw_writer *w,
                              const struct yoctonw_buffer *name);
+
+/**
+ * Start writing a new subobject.
+ *
+ * The @ref yoctonw_end function should be called to end the subobject.
+ *
+ * @param w       Writer.
+ * @param name    Field name for subobject.
+ */
 void yoctonw_subobject(struct yoctonw_writer *w, const char *name);
+
+/**
+ * End the current subobject.
+ *
+ * @param w       Writer.
+ */
 void yoctonw_end(struct yoctonw_writer *w);
+
+/**
+ * Check if an error occurred.
+ *
+ * @return    Non-zero if an error occurred during output (ie. the output
+ *            callback function returned zero).
+ */
 int yoctonw_have_error(struct yoctonw_writer *w);
 
