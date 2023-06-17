f386272baceb21650c730f63be6d32ff319fd8cf
Simon Howard <fraggle@soulsphere.org>
2023-06-17 11:54:36 -0400

    Initial API documentation

---
+++ mainpage.h
@@ -0,0 +1,10 @@
+/**
+ * @mainpage Yocton API
+ *
+ * The following is some documentation for how to use the Yocton API.
+ *
+ * - @link parse_api Parse API @endlink - introduction to the parsing API
+ * - @link yocton.h Parse API functions @endlink
+ * - @link yoctonw.h Write API functions @endlink
+ *
+ */
