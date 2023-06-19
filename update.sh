14c1fc8006922af44262920d913cefbd014e8592
Simon Howard <fraggle@soulsphere.org>
2023-06-19 14:45:35 -0400

    Fully automate doc updates.

--- update.sh
+++ update.sh
@@ -4,10 +4,14 @@ set -eu
 
 git submodule update --checkout yocton
 cd yocton
+git checkout trunk
 git pull
+head_commit=$(git show-ref HEAD | sed "s/ .*//")
 make docs
 cd ..
-git rm *.{css,html,js,png}
+git rm -f *.{css,html,js,png}
 cp -R yocton/html/* .
 git add *.{css,html,js,png}
+git add yocton
+git commit -m "Update doxygen documentation to commit $head_commit."
 
